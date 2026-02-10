ed390331ab6da9141bacf64e2cec273214effdb9
Ruan Haack <ruan.haack03@gmail.com>
2026-02-09 22:53:58 -0300

    Category

---
+++ src/main/java/com/haackdev/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.haackdev.course.repositories;
+
+import com.haackdev.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
