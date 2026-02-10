b312fd76ddf71be33aae1bc21a6dff5d577306aa
Ruan Haack <ruan.haack03@gmail.com>
2026-02-09 23:08:34 -0300

    Product

---
+++ src/main/java/com/haackdev/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.haackdev.course.repositories;
+
+import com.haackdev.course.entities.Category;
+import com.haackdev.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ src/main/java/com/haackdev/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.haackdev.course.services;
+
+import com.haackdev.course.entities.Product;
+import com.haackdev.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> FindAll(){
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
