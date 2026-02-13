f1d7235e0a0a82f53db2e87e380769b7ef2d6d2b
Ruan Haack <ruan.haack03@gmail.com>
2026-02-12 23:17:50 -0300

    Exception handling - findById

--- src/main/java/com/haackdev/course/services/UserService.java
+++ src/main/java/com/haackdev/course/services/UserService.java
@@ -2,9 +2,11 @@ package com.haackdev.course.services;
 
 import com.haackdev.course.entities.User;
 import com.haackdev.course.repositories.UserRepository;
+import com.haackdev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.ReadOnlyFileSystemException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
